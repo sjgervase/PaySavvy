a292cafb636fc7a8dd50c052cda77017e2b6e623
Stephen Gervase <stephenjgervase@icloud.com>
2024-04-12 14:51:29 -0400

    es lint config fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,5 +5,6 @@ module.exports = {
     'plugin:react/jsx-runtime',
     '@electron-toolkit/eslint-config-ts/recommended',
     '@electron-toolkit/eslint-config-prettier'
-  ]
+  ],
+  rules: { 'prettier/prettier': ['error', { endOfLine: 'auto' }] }
 }
